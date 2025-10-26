b69c779f974175bb0bc165f2866017a2f0fdd2b0
othmanmanasrah95 <othmanmanasrah95@gmail.com>
2025-10-26 08:05:40 +0200

    strip first run

--- services/stripeService.js
+++ services/stripeService.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY || 'sk_test_placeholder');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY );
 
 class StripeService {
   constructor() {
